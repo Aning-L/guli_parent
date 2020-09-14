c01433edc0df0515a7a3f2f79e2c80889a80635c
aning <aning.@ln.com>
2020-09-14 14:39:25 +0800

    第二次提交h

--- service/src/main/java/com/atguigu/git/GitTest.java
+++ service/src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("0.0");
         System.out.println("测试冲突hotfix");
         System.out.println("0.0");
+        System.out.println("第二次提交h");
     }
 }
