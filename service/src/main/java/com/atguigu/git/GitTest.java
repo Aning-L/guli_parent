e8d21988a62acde9bec7d0836f6f3859f59a5ba9
aning <aning.@ln.com>
2020-09-14 14:36:25 +0800

    测试冲突 master

--- service/src/main/java/com/atguigu/git/GitTest.java
+++ service/src/main/java/com/atguigu/git/GitTest.java
@@ -3,7 +3,7 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("0.0");
-        System.out.println("0.0");
+        System.out.println("测试冲突 master");
         System.out.println("0.0");
     }
 }
